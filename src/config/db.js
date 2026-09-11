f029e5a7a38105dbe94ff759d5a43e35c5a51470
unknown <juvilynmagante12@gmail.com>
2026-09-11 12:48:05 +0800

    activity 3

--- src/config/db.js
+++ src/config/db.js
@@ -1,1 +1,20 @@
-,
+import pg from "pg";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const { Pool } = pg;
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+});
+
+pool.on("connect", () => {
+  console.log("Connected to PostgreSQL database");
+});
+
+pool.on("error", (err) => {
+  console.log("Unexpected PostgreSQL error:", err);
+});
+
+export default pool;
